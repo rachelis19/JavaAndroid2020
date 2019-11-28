ca2c2ca18e3add87050d4eda2a989544a589af14
rachelis19 <44132906+rachelis19@users.noreply.github.com>
2019-11-28 17:57:22 +0200

    second push

---
+++ app/src/main/java/com/example/service_side/View/User_Details.java
@@ -0,0 +1,20 @@
+package com.example.service_side.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.service_side.R;
+
+public class User_Details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user__details);
+        Intent i=getIntent();
+
+
+    }
+}
---
+++ app/src/main/java/com/example/service_side/View/package_details.java
@@ -0,0 +1,15 @@
+package com.example.service_side.View;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.service_side.R;
+
+public class package_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_package_details);
+    }
+}
